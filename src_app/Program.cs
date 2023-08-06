55243f354adba80589f483ec64c2a5e7443a175d
eSqadron <kubam0906@gmail.com>
2023-08-06 17:43:44 +0200

    list serial ports

--- src_app/Program.cs
+++ src_app/Program.cs
@@ -18,36 +18,11 @@ namespace Chasztag_sender_app
             implementations.Add(new HostappBackendSerial());
             implementations.Add(new HostappBackendBluetooth());
 
-            int i = 1;
+            int i = 0;
 
             implementations[i].list_devices();
+            implementations[i].init_connection();
 
-            // UInt32 set = 10000;
-            // byte[] s = new byte[6];
-            
-            // s[0] = 0x21;
-
-            // for(int i =4; i> 0; --i){
-            //     s[i] = Convert.ToByte(set & 0xff);
-            //     set = set >> 8;
-            // }
-
-            // s[5] = Convert.ToByte('\n');
-            
-
-            // SerialPort mySerialPort = new SerialPort("COM9");
-            // mySerialPort.BaudRate = 9600;
-
-            // mySerialPort.Open();
-
-            // mySerialPort.Write(s, 0, s.Length);
-
-            // mySerialPort.Close();
-
-            // foreach(var item in s)
-            // {
-            //     Console.WriteLine(item.ToString());
-            // }
         }
     }
 }
--- src_app/backend/callback_serial.cs
+++ src_app/backend/callback_serial.cs
@@ -1,10 +1,70 @@
+using System.IO.Ports; //dotnet add package System.IO.Ports --version 7.0.0
+
 using driver_hostapp.backend.callback_interface;
 
 namespace driver_hostapp.backend.callback_serial{
+
+    // public class NotNordicDevice : Exception
+    // {
+
+    //     public EmployeeListNotFoundException(string message)
+    //         : base(message)
+    //     {
+    //     }
+    // }
+
     public class HostappBackendSerial : IHostappBackend{
 
         public void list_devices(){
-            Console.WriteLine("serial init");
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("The following serial ports were found:");
+
+            // Display each port name to the console.
+            foreach(string port in ports)
+            {
+                Console.WriteLine(port);
+                this.get_serial_device_info(port);
+            }
+            
+        }
+
+        private void get_serial_device_info(string port_name){
+            var str_to_write = "hwinfo devid\n";            
+
+            SerialPort mySerialPort = new SerialPort(port_name);
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.RtsEnable = true; // thefuck?
+            mySerialPort.DtrEnable = true; // thefuck?
+
+            mySerialPort.ReadTimeout = 100;
+
+            mySerialPort.Open();
+
+            mySerialPort.Write(str_to_write);
+
+            string final_id = "";
+
+            for (int i = 0; i < 3; i++)
+            {
+                try{
+                    string l = mySerialPort.ReadLine();
+                    if(l.Contains("ID: ")){
+                        Console.WriteLine(l);
+                    }
+                }
+                catch (System.TimeoutException)
+                {
+
+                }
+                
+            }
+
+            // if(final_id == ""){
+            //     throw
+            // }
+
+            mySerialPort.Close();
         }
 
         public void init_connection(){
