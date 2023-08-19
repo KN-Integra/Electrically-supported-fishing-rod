53546df6e8d3744c3158eb4a244668d542f9d876
Maciej Baczmanski <maciej.baczmanski@nordicsemi.no>
2023-08-19 16:22:14 +0200

    fixed usb initialization for dongle

--- src_firmware/src/main.c
+++ src_firmware/src/main.c
@@ -1,13 +1,12 @@
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/device.h>
+#include <zephyr/usb/usb_device.h>
 
 #include <string.h>
 
 
 void main(void)
 {
-
+	usb_enable(NULL);
 	while (1) {
 
 		k_sleep(K_SECONDS(1U));
