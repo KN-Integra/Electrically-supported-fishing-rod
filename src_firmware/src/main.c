b193686f3c4237e05051b4f0262939dbcaaa4a92
eSqadron <kubam0906@gmail.com>
2023-08-26 18:11:49 +0200

    Add Printk configuration to main

--- src_firmware/src/main.c
+++ src_firmware/src/main.c
@@ -1,14 +1,35 @@
 #include <zephyr/kernel.h>
+
 #include <zephyr/usb/usb_device.h>
 
 #include <string.h>
 
 #include "driver.h"
+#include <zephyr/sys/printk.h>
 
+#include <zephyr/drivers/uart.h>
 
 void main(void)
 {
-	usb_enable(NULL);
+
+	const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+	if (usb_enable(NULL)) {
+		return;
+	}
+
+	/* Poll if the DTR flag was set */
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		/* Give CPU resources to low priority threads. */
+		k_sleep(K_MSEC(100));
+	}
+
+	int console_init(void);
+	printk("hello world");
+
+
 	motor_off();
 	while (1) {
 
